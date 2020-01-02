307222b5fcef45c030bf6d6c4c65585fe0fb3cf6
Elijah Broaddus <elijahlb@gmail.com>
2020-01-02 18:56:27 -0500

    ajax update

--- Elijah-Docs/app.js
+++ Elijah-Docs/app.js
@@ -1,16 +1,49 @@
+$(document).ready(function() {
 $(document).foundation() 
-$( document ).ready(function() {
+$('#random-button').on('click', function(e) {
+   e.preventDefault();
    
-    var search = "old fashioned"
-    var queryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + search + "&limit=1&namespace=0&format=jsonfm"
+
+  var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
+        
           console.log(response);
+          var img = response.drinks[0].strDrinkThumb;
+          $('#drink-img').attr('src', img)
+          $('.bev-title').html(response.drinks[0].strDrink)
+          $('.glassware').html(response.drinks[0].strGlass)
+          $('.instructions').html(response.drinks[0].strInstructions)
 
+          // APPENDING INGREDIENTS
+          var measureObj = response.drinks[0];
+          $('.recipe-measure').empty();
+          Object.keys(measureObj).forEach(function(key) {
+          if (key.charAt(3) == 'M' && measureObj[key] != null || "") {
+          $('.recipe-measure').append(measureObj[key] + ": " + "<br>");
+          }
+        });
+          var nameObj = response.drinks[0];
+          $('.recipe-writing').empty();
+            Object.keys(nameObj).forEach(function(key) {
+            if (key.charAt(5) == 'g' && key.charAt(6) == 'r' && nameObj[key] != null) {
+            $('.recipe-writing').append(nameObj[key] + "<br>");
+            }
+          });
+
+        //RECIPE SAVE
+        $('.recipe-add').on('click', function(e) {
+        e.preventDefault();
+    
+        var drinkId = JSON.parse(response.drinks[0].idDrink);
+        localStorage.setItem("savedrecipes", drinkId);     
+        });
 
   });
+});
+
 
 });
